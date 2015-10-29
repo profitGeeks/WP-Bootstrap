73ab64e862dde609da74e02748c64b4c6c64f202
Rick Wiley <rickwiley7@gmail.com>
2015-10-29 14:48:15 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+ProfitGeeks 1.0 Wordpress Bootstrap Theme
+===================
+
+No frills. Uses Bootstrap 3.3.4 
+
+Compile CSS using library/wp.less to wp.css
+
---
+++ header.php
@@ -0,0 +1,80 @@
+<!doctype html>  
+
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+	
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+  		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/icons/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon144.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon114.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon72.png">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/icons/icon57.png">
+       
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+		<!-- IE8 fallback moved below head to work properly. Added respond as well. Tested to work. -->
+			<!-- media-queries.js (fallback) -->
+		<!--[if lt IE 9]>
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+		<![endif]-->
+
+		<!-- html5.js -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->	
+		
+			<!-- respond.js -->
+		<!--[if lt IE 9]>
+		          <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
+		<![endif]-->	
+	</head>
+	
+	<body <?php body_class(); ?>>
+				
+		<header role="banner">
+				
+			<div class="navbar navbar-default navbar-fixed-top">
+			<?php   // Fix menu overlap bug..
+                 if ( is_admin_bar_showing() ) echo '<div style="min-height: 32px;"></div>'; 
+            ?>
+			
+				<div class="container">
+          
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+
+						<a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+					</div>
+
+					<div class="collapse navbar-collapse navbar-responsive-collapse">
+						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+
+						<?php //if(of_get_option('search_bar', '1')) {?>
+						<form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+							<div class="form-group">
+								<input name="s" id="s" type="text" class="search-query form-control" autocomplete="off" placeholder="<?php _e('Search','wpbootstrap'); ?>">
+							</div>
+						</form>
+						<?php //} ?>
+					</div>
+
+				</div> <!-- end .container -->
+			</div> <!-- end .navbar -->
+		
+		</header> <!-- end header -->
+		
+		<div class="container">
